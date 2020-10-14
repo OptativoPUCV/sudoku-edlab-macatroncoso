fbebf26acf6d8df018a7055c2259ccda95de1866
repl.it user <replituser@example.com>
2020-10-14 18:23:40 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -68,6 +68,14 @@ List* list=createList();
 
 
 int is_final(Node* n){
+  int i,j; 
+   for(i=0;i<9;i++){
+     for(j=0;j<9;j++){
+       if(n->sudo[i][j] == 0){
+        return 1;
+          }
+       }
+   }
     return 0;
 }
 
