6e8a9764fc97f188ec616feb418b083795b418ad
repl.it user <replituser@example.com>
2020-10-19 01:45:18 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -43,30 +43,26 @@ void print_node(Node* n){
 
 int is_valid(Node* n){
     int i, j, purru;
-    
-    for (i=0; i<9; i++){
-      for (j=0; j<9; j++){ 
-        if (n->sudo[i][j] == 0) continue;
-
-             purru= 3*(i/3) + (j/3);
-        for(int p=0;p<9;p++){
-            int iwi= 3*(purru/3) + (p/3) ;
-            int owo= 3*(purru%3) + (p%3) ;
-            if ((i!=iwi && j!=owo) && n->sudo[i][j] == (n->sudo[iwi][owo]) ){
-              return 0;
-            }
-        }        
-      }
-
-        for (purru=0; purru<9; purru++){
-            if (n->sudo[i][j] == n->sudo[i][purru] && j != purru){
-              return 0;
-            }
-            if (n->sudo[i][j] == n->sudo[purru][j] && i != purru){
-              return 0;
-            }
+ for (i=0; i<9; i++){
+   for (j=0; j<9; j++){ 
+   if (n->sudo[i][j] == 0) continue;
+     for (purru=0; purru<9; purru++){
+      if (n->sudo[i][j] == n->sudo[i][purru] && j != purru){
+        return 0;
         }
-
+      if (n->sudo[i][j] == n->sudo[purru][j] && i != purru){
+          return 0;
+        }
+       }
+ purru= 3*(i/3) + (j/3);
+ for(int p=0;p<9;p++){
+   int iwi= 3*(purru/3) + (p/3) ;
+    int owo= 3*(purru%3) + (p%3) ;
+   if ((i!=iwi && j!=owo) && n->sudo[i][j] == (n->sudo[iwi][owo]) ){
+        return 0;
+       }
+      }        
+      }
     }
   return 1;
 }
